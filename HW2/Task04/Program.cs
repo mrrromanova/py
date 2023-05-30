bf055e65dd6f30ecfd784084bf46db05bee9869b
mashkamashka <romanova98ma@mail.ru>
2023-05-30 18:59:07 +0300

    commit

---
+++ HW2/Task04/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число");
+int num1 = int.Parse(Console.ReadLine());
+int number = 1;
+while (number<num1)
+{
+int Y = number % 2;
+if (Y==0)
+{
+    Console.Write($"{number}, ");
+}
+}
+
+while (number==num1)
+{
+int Y = number % 2;
+if (Y==0)
+{
+    Console.Write($"{number}.");
+}
+}
+
+number = number + 1;
---
+++ HW2/Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int X = int.Parse(Console.ReadLine());
+int number = 1;
+while (number<X)
+{
+int Y = number % 2;
+if (Y==0)
+{
+    Console.Write($"{number}, ");
+}
+}
+
+while (number==X)
+{
+int Y = number % 2;
+if (Y==0)
+{
+    Console.Write($"{number}.");
+}
+}
+
+number = number + 1;
