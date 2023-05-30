2898ab9fd6594278d51eb69365322209a0c02a1a
mashkamashka <romanova98ma@mail.ru>
2023-05-30 18:37:28 +0300

    first commit

---
+++ HW2/Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int number1 = number % 2;
+
+if (number1 == 0)
+{
+    Console.WriteLine($"Число {number} является чётным");
+}
+
+else
+{
+    Console.WriteLine($"Число {number} является нечётным");
+}
